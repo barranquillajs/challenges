b9b4d55825acdc0754078d6403f83c365745ff50
Eder Diaz <ederdiaz_@hotmail.com>
2018-10-20 18:29:49 -0500

    Solucion 1

---
+++ ch0/solucion.js
@@ -0,0 +1,10 @@
+
+function solucion (obj) { return  Object.keys(obj); }
+
+let obj = {
+    0: { name: 'Jon', lastname: 'Doe' },
+    1: { name: 'Linus', lastname: 'Torlvalds' },
+    2: { name: 'Mark', lastname: 'Zuckerberg' }
+  }
+
+  solucion(obj);
